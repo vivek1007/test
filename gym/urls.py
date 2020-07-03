32599a24a641eb41e53b8aac6c05351e90262565
vivek <tomarvvk1007@gmail.com>
2020-07-03 13:58:21 +0530

    add all files

---
+++ gym/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+    path('notifications/', include('notifications.urls')),
+    path('reports/', include('reports.urls')),
+    path('', include('accounts.urls'))
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
